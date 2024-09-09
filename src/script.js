81f6c8fee985a0e74c03f718bb1bc3cfdd2831f9
Kerwin Cordero <kerwinmichael24@gmail.com>
2024-09-09 13:58:46 -0400

    colores y titulos modificados

--- src/script.js
+++ src/script.js
@@ -2,5 +2,5 @@ const boton_1 = document.getElementById('cambiarColor_1')
 
 boton_1.addEventListener('click', () => {
   const body = document.querySelector('body')
-  body.style.backgroundColor = 'yellow'
+  body.style.backgroundColor = 'blue'
 })
